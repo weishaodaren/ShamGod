5b0873be523112b87fea28d1b9b4430b5d514990
weishaodaren <weishaodaren@gmail.com>
2021-07-13 19:33:13 +0800

    template -

---
+++ Basic/string.py
@@ -0,0 +1,19 @@
+# s3 = ''''''
+# print(s3)
+
+# s1 = '\'hello, world!\''
+# print(s1)
+
+# s2 = '\n\\hello, world!\\\n'
+# print(s2)
+
+# s1 = r'\'hello, world!\''
+# s2 = r'\n\\hello, world!\\\n'
+# print(s1, s2, end='')
+
+s1 = 'hello' * 3
+s2 = 'kitty'
+s1 += s2
+
+# print('k' in s1)
+print(s1[2:10])
