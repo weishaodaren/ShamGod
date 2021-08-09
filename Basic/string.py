74b5332b493b4f5e5084a46e21ee14c21f39a601
weishaodaren <weishaodaren@gmail.com>
2021-08-09 13:39:56 +0800

    update

--- Basic/string.py
+++ Basic/string.py
@@ -36,3 +36,40 @@ print(str1.strip())
 a, b = 5, 10
 # py 3.6
 print(f'{a} * {b} = {a * b}')
+
+list1 = [1, 3, 5, 7, 100]
+print(len(list1))
+
+for index in range(len(list1)):
+    print(list1[index])
+for elem in list1:
+    print(elem)
+for index, elem in enumerate(list1):
+    print(index, elem)
+
+list1.append(200)
+list1.insert(1,400)
+list1 +=[1000, 2000]
+
+if 3 in list1:
+    list1.remove(3)
+if 1234 in list1:
+    list1.remove(1234)
+
+list1.pop(0)
+list1.clear()
+print(list1)
+
+
+fruits = ['grape', 'apple', 'strawberry', 'waxberry']
+fruits += ['pitaya', 'pear', 'mango']
+print(fruits[1:4])
+print(fruits[:])
+print(fruits[::-1])
+
+
+list2 = ['orange', 'apple', 'zoo', 'internationalization', 'blueberry']
+list3 = sorted(list2, reverse=True)
+list4 = sorted(list2, key=len)
+print(list3)
+print(list4)
