15128df00ac0e2e0dd65b6fce9d0db70025edc6c
weishaodaren <weishaodaren@gmail.com>
2021-10-12 10:23:08 +0800

    override

---
+++ Basic/override.py
@@ -0,0 +1,36 @@
+from abc import ABCMeta, abstractmethod
+
+class Pet(object, metaclass=ABCMeta):
+    """宠物"""
+
+    def __init__(self, nickname):
+        self._nickname = nickname
+    
+    @abstractmethod
+    def make_voice(self):
+        """发出声音"""
+        pass
+
+
+class Dog(Pet):
+    """狗"""
+
+    def make_voice(self):
+        print('%s: 汪汪汪...' % self._nickname)
+
+
+class Cat(Pet):
+     """猫"""
+
+     def make_voice(self):
+         print('%s: 喵。。。喵' % self._nickname)
+
+
+def main():
+    pets = [Dog('憨憨'), Cat('尿尿'), Dog('旺财')]
+    for pet in pets:
+        pet.make_voice()
+
+
+if __name__ == '__main__':
+    main()
