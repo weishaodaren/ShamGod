f5e9afb6069705e3ecae8fe3973509040295462a
weishaodaren <weishaodaren@gmail.com>
2021-10-13 14:40:08 +0800

    屏蔽敏感字

---
+++ Regex/case3.py
@@ -0,0 +1,10 @@
+from re import sub, IGNORECASE
+
+def main():
+    sentence = '你丫是傻叉吗? 我操你大爷的. Fuck you.'
+    purified = sub('[操肏艹]|fuck|shit|傻[比屄逼叉缺吊屌]|煞笔',
+                      '*', sentence, flags=IGNORECASE)
+    print(purified)
+
+if __name__ == '__main__':
+    main()
